f2fc0118b74fe8dc34d9952abdace7a520c652be
Stefan Weckend <stefan.weckend@arcor.de>
2016-11-18 15:45:06 +0100

    calculating worktime

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Person;
+use AppBundle\Entity\Worktime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\CsvHandler;
@@ -34,9 +34,29 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $repository_user = $em->getRepository('AppBundle:Person');
         $repository_workday = $em->getRepository('AppBundle:Workday');
+        $repository_worktime = $em->getRepository('AppBundle:Worktime');
         $user = $repository_user->find($user_id);
         $workdays = $repository_workday->findBy(array('person' => $user_id));
 
-        return $this->render('user.html.twig', array('user' => $user, 'workdays' => $workdays));
+        $workday_data = [];
+        foreach ($workdays as $workday) {
+            $worktimes = $repository_worktime->findBy(array('workday' => $workday->getId()));
+            $time = 0;
+            /* @var $worktime \AppBundle\Entity\Worktime */
+            foreach ($worktimes as $worktime) {
+                $time_from = $worktime->getTimeFrom();
+                $time_to = $worktime->getTimeTo();
+                $time += $time_to->getTimestamp() - $time_from->getTimestamp();
+            }
+            $dt_from = new \DateTime();
+            $dt_from->setTimestamp(0);
+            $dt_to = new \DateTime();
+            $dt_to->setTimestamp($time);
+            $diff = $dt_from->diff($dt_to);
+            $time_string = $diff->format('%h:%I');
+            $workday_data[] = [$workday, $time_string];
+        }
+
+        return $this->render('user.html.twig', array('user' => $user, 'workday_data' => $workday_data));
     }
 }
