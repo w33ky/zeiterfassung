2fd66a73f225dca06650b98d36a33c3ce756275c
Stefan Weckend <stefan.weckend@arcor.de>
2016-11-18 14:37:59 +0100

    list workdays

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Person;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\CsvHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class DefaultController extends Controller
@@ -22,5 +24,19 @@ class DefaultController extends Controller
         $users = $repository->findAll();
 
         return $this->render('csv.html.twig', array('users' => $users));
+    }
+
+    /**
+     * @Route("/user/{user_id}", name="user")
+     */
+    public function userAction($user_id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repository_user = $em->getRepository('AppBundle:Person');
+        $repository_workday = $em->getRepository('AppBundle:Workday');
+        $user = $repository_user->find($user_id);
+        $workdays = $repository_workday->findBy(array('person' => $user_id));
+
+        return $this->render('user.html.twig', array('user' => $user, 'workdays' => $workdays));
     }
 }
